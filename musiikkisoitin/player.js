45d70bec5a7d2cb5e2e310c8a8f060382da4a59c
“Erik <erikhuuskonen@kamk.fi>
2020-04-21 20:24:49 +0300

    edits

--- musiikkisoitin/player.js
+++ musiikkisoitin/player.js
@@ -1,88 +1,95 @@
-
-/*function PlayPause() {
-    var x = document.getElementById('play');
-    var y = document.getElementById('stop');
-    if (x.className == "fa fa-play") {
-        x.className = "fa fa-pause";
-    }
-    else if (x.className == "fa fa-pause") {
-        x.className = "fa fa-play";
-    }
-}
-*/
-function hello() {
+/*function hello() {
     alert('You suck');
 }
+*/
 
 
-var Mediaplayer = function() {
+var Mediaplayer = function () {
     //Jqueryllä on suht helppo tehdä juttuja etsimällä id:llä, tämän opin tuossa Presidentti 2 tehtävässä.
     $('#pause').hide();
 
     var audio = document.getElementById('audio');
-    //var currentTime = document.getElementById('progress');
-    var songSlider = document.getElementById('songSlider');
- //setInterval(updateSongSlider, 1000);
 
-    if (!audio.currentTime) {
+    //Jostain syystä, joitakin ei voi Jquerettaa, mutta joitakin voi(?)
+    var songSlider = $('#songSlider');
+    //songSlider.setAttribute("max", Math.round(audio.duration));
+    setInterval(updateSongSlider, 1000);
+
+    /*if (!audio.currentTime) {
         $('#duration').html('0.00');
-    }
-    
+    }*/
+
     var songs = ["https://sound.peal.io/ps/audios/000/017/265/original/youtube_17265.mp3?1533627695",
-                "http://opengameart.org/sites/default/files/Soliloquy_1.mp3",
-				 "http://opengameart.org/sites/default/files/Sigil_3.ogg",
-                 "http://opengameart.org/sites/default/files/sadorchestralbgm%28syncopika%29.wav"];
+        "http://opengameart.org/sites/default/files/Soliloquy_1.mp3",
+        "http://opengameart.org/sites/default/files/Sigil_3.ogg",
+        "http://opengameart.org/sites/default/files/sadorchestralbgm%28syncopika%29.wav"];
 
     var songTitle = document.getElementById('title');
     var currentSong = 0;
     window.onload = loadSong(), showDuration();
-  
-    function loadSong() {
-        audio.src = songs[currentSong];
-        document.getElementById('nowplaying').innerHTML = songs[currentSong];
-        //setTimeout(showDuration, 1000);
-    }
-                 /* Lataa ensimmäinen biisi */
 
-	// Listaa biisit
-	for (var song in songs) {
+    function loadSong() {
+        var currentSong = 0;
+        audio.src = $("#playlist li a")[0];
+        $('#pause').hide();
+        $('#play').show();
+        setTimeout(showDuration, 1000);
+        skipSong();
+    }
+    function skipSong() {
+        $("#playlist li a").click(function (event) {
+            event.preventDefault();
+            audio.src = this;
+            $('#pause').hide();
+            $('#play').show();
+            $("#playlist li").removeClass("current-song");
+            currentSong = $(this).parent().index();
+            $(this).parent().addClass("current-song");
+            document.getElementById('nowplaying').innerHTML = currentSong;
+        });
+    }
+    /* Lataa ensimmäinen biisi */
+
+    // Listaa biisit
+    for (var song in songs) {
         var songTitle = document.getElementsByClassName('link');
         songTitle.innerHTML += songs[song] + '<br />';
-		// Mahdollista myös biisin valinta, esim. painamalla biisin nimeä !
-	}
+        // Mahdollista myös biisin valinta, esim. painamalla biisin nimeä !
+    }
 
-	/* Alusta mediasoitin */
+    /* Alusta mediasoitin */
     initMediaplayer();
-    var songSlider = document.getElementById("songSlider");
+    var songSlider = document.getElementById('songSlider');
+    //songSlider.max = "audio.duration";
 
-	/* Alusta soitin, luo tapahtumat */
-	function initMediaplayer() {
-		document.getElementById('play').addEventListener('click', playMusic, false);
-        document.getElementById('stop').addEventListener('click', stopMusic, false);
-        document.getElementById('pause').addEventListener('click', pauseMusic, false);
+    /* Alusta soitin, luo tapahtumat */
+    function initMediaplayer() {
+        document.getElementById('play').addEventListener('click', play, false);
+        document.getElementById('stop').addEventListener('click', stop, false);
+        document.getElementById('pause').addEventListener('click', pause, false);
         document.getElementById('previous').addEventListener('click', previous, false);
         document.getElementById('next').addEventListener('click', next, false);
-        document.getElementById('volume').addEventListener('click', Volume, false);
+        //document.getElementById('volume').addEventListener('click', Volume, false);
         document.getElementById('songSlider').addEventListener('click', seekSong, false);
-        document.getElementById('songSlider').addEventListener = function() {
+        document.getElementById('songSlider').addEventListener = function () {
             this.style.background = 'linear-gradient(to right, #82CFD0 0%, #82CFD0 ' + this.value + '%, #fff ' + this.value + '%, white 100%)'
-          };
+        };
         //output.innerHTML = 0;
-		/* Lisää myös muut tapahtumat */
+        /* Lisää myös muut tapahtumat */
     }
+
+
     function updateSongSlider() {
         var c = Math.round(audio.currentTime);
         songSlider.value = c;
         currentTime = convertTime(c);
         audio.addEventListener('timeupdate');
         var position = audio.currentTime / audio.duration;
-        songSlider.style.width = position * audio.duration + '%';
-        document.getElementById("songSlider") = audio.duration;
-
+        songSlider.style.width = position * 100 + '%';
     }
 
-    function convertTime (secs) {
-        var min = Math.floor(secs/60);
+    function convertTime(secs) {
+        var min = Math.floor(secs / 60);
         var sec = secs % 60;
         min = (min < 10) ? '0' + min : min;
         sec = (sec < 10) ? '0' + sec : sec;
@@ -91,10 +98,11 @@ var Mediaplayer = function() {
     function seekSong() {
         audio.currentTime = songSlider.value;
         currentTime = convertTime(audio.currentTime);
+        playMusic()
     }
 
-    function showDuration(){
-        $(audio).bind('timeupdate', function(){
+    function showDuration() {
+        $(audio).bind('timeupdate', function () {
             //Get hours and minutes
             var sec = parseInt(audio.currentTime % 60);
             var min = parseInt((audio.currentTime / 60) % 60);
@@ -104,51 +112,38 @@ var Mediaplayer = function() {
             if (sec < 10) {
                 sec = '0' + sec;
             }
-            $('#duration').html(min + '.' + sec);	
+            $('#duration').html(min + '.' + sec);
             var value = 0;
             if (audio.currentTime > 0) {
                 value = Math.floor((100 / audio.duration) * audio.currentTime);
             }
             $('#durationFull').html('/ ' + minFull + '.' + secFull);
-            $('#progress').css('width',value+'%');
+            $('#progress').css('width', value + '%');
         });
     }
-    function Volume() {
-        var slider = document.getElementById('volume');
-        var output = document.getElementById('volumevalue');
-        var audio = document.getElementById('audio')
-        output.innerHTML = slider.nodeValue;
-
-        slider.onchange = function () {
-            output.innerHTML = this.value;
-            //Koska range on 0-10, mutta volume toimii 0-1, pitää siis jakaa 10, joten saadan esim. 5 / 10 = 0.5
-            audio.volume = this.value / 10;
-        }
-    }
-	/* Play-toiminto */
-	function playMusic() {
+    /* Play-toiminto */
+    function play() {
         audio.play();
         $('#play').hide();
         $('#pause').show();
+        document.getElementById('nowplaying').innerHTML = currentSong;
         showDuration();
-	}
+        updateSongSlider();
+    }
 
-	/* Stop-toiminto */
-	function stopMusic() {
-		audio.pause();
+    /* Stop-toiminto */
+    function stop() {
+        audio.pause();
         audio.currentTime = 0;
         $('#pause').hide();
         $('#play').show();
-	}
+    }
 
-	function pauseMusic() {
+    function pause() {
         audio.pause();
         audio.currentTime = audio.currentTime;
         $('#pause').hide();
         $('#play').show();
-    }
-    function volume() {
-        
     }
     function next() {
         currentSong = currentSong + 1 % songs.length;
@@ -156,6 +151,7 @@ var Mediaplayer = function() {
         $('#play').show();
         loadSong()
     }
+
     function previous() {
         currentSong--;
         currentSong = (currentSong < 0) ? songs.lenght - 1 : currentSong;
