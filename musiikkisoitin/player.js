598ffa8525899681ffc4545b59a0dba35c602e8a
“Erik <erikhuuskonen@kamk.fi>
2020-04-21 11:12:23 +0300

    edits

--- musiikkisoitin/player.js
+++ musiikkisoitin/player.js
@@ -1,4 +1,5 @@
-function PlayPause() {
+
+/*function PlayPause() {
     var x = document.getElementById('play');
     var y = document.getElementById('stop');
     if (x.className == "fa fa-play") {
@@ -8,109 +9,127 @@ function PlayPause() {
         x.className = "fa fa-play";
     }
 }
+*/
 function hello() {
     alert('You suck');
 }
-function duration()
-{
-    var audio = document.getElementById('audio');
-    var d = document.getElementById('duration');
-    if(audio.readyState > 0) 
-    {
-        var minutes = parseInt(audio.duration / 60, 10);
-        var seconds = parseInt(audio.duration % 60);
-        
-        d.html(minutes+":"+seconds);
-    }
-}
-/* Luo mediaplayer-olio */
+
 
 var Mediaplayer = function() {
-    //Jqueryllä on suht helppo tehdä juttuja etsimällä id:llä
+    //Jqueryllä on suht helppo tehdä juttuja etsimällä id:llä, tämän opin tuossa Presidentti 2 tehtävässä.
     $('#pause').hide();
 
     var audio = document.getElementById('audio');
+    //var currentTime = document.getElementById('progress');
+    var songSlider = document.getElementById('songSlider');
+ //setInterval(updateSongSlider, 1000);
 
     if (!audio.currentTime) {
         $('#duration').html('0.00');
     }
     
-
-	var songs = ["http://opengameart.org/sites/default/files/Soliloquy_1.mp3",
+    var songs = ["https://sound.peal.io/ps/audios/000/017/265/original/youtube_17265.mp3?1533627695",
+                "http://opengameart.org/sites/default/files/Soliloquy_1.mp3",
 				 "http://opengameart.org/sites/default/files/Sigil_3.ogg",
-				 "http://opengameart.org/sites/default/files/sadorchestralbgm%28syncopika%29.wav",];
+                 "http://opengameart.org/sites/default/files/sadorchestralbgm%28syncopika%29.wav"];
 
     var songTitle = document.getElementById('title');
     var currentSong = 0;
-    window.onload = loadSong()
-
+    window.onload = loadSong(), showDuration();
+  
     function loadSong() {
         audio.src = songs[currentSong];
+        document.getElementById('nowplaying').innerHTML = songs[currentSong];
+        //setTimeout(showDuration, 1000);
     }
                  /* Lataa ensimmäinen biisi */
 
 	// Listaa biisit
 	for (var song in songs) {
-		document.getElementById('playList').innerHTML += songs[song] + '<br />';
+        var songTitle = document.getElementsByClassName('link');
+        songTitle.innerHTML += songs[song] + '<br />';
 		// Mahdollista myös biisin valinta, esim. painamalla biisin nimeä !
 	}
 
 	/* Alusta mediasoitin */
-	initMediaplayer();
+    initMediaplayer();
+    var songSlider = document.getElementById("songSlider");
 
 	/* Alusta soitin, luo tapahtumat */
 	function initMediaplayer() {
-        document.getElementsByClassName('title');
-        document.getElementsByClassName('artist');
 		document.getElementById('play').addEventListener('click', playMusic, false);
         document.getElementById('stop').addEventListener('click', stopMusic, false);
         document.getElementById('pause').addEventListener('click', pauseMusic, false);
         document.getElementById('previous').addEventListener('click', previous, false);
         document.getElementById('next').addEventListener('click', next, false);
-
-        document.getElementById('duration').addEventListener(duration, false);
-
-        document.getElementById('volume').addEventListener(volume, false);
+        document.getElementById('volume').addEventListener('click', Volume, false);
+        document.getElementById('songSlider').addEventListener('click', seekSong, false);
+        document.getElementById('songSlider').addEventListener = function() {
+            this.style.background = 'linear-gradient(to right, #82CFD0 0%, #82CFD0 ' + this.value + '%, #fff ' + this.value + '%, white 100%)'
+          };
+        //output.innerHTML = 0;
 		/* Lisää myös muut tapahtumat */
     }
-    
-    function Title() {
-        songTitle.textContent = (currentSong + 1), '. ', songs[currentSong];
-        nextSongTitle.innerHTML = "<b>Next Song: </b>", songs[currentSong + 1 % songs.length];
-        }
-
-    audio.addEventListener('timeupdate', function() {
-        var expandingBart = document.getElementById("progress");
+    function updateSongSlider() {
+        var c = Math.round(audio.currentTime);
+        songSlider.value = c;
+        currentTime = convertTime(c);
+        audio.addEventListener('timeupdate');
         var position = audio.currentTime / audio.duration;
-        expandingBar.style.width = postion * 100 + '%';
-        convertTime(Math.round(audio.currentTime));
-        if(audio.ended) {
-            this.next();
-        }
-    });
-    function convertTime(seconds) {
-        var min = Math.floor(seconds / 60);
-        var sec = seconds % 60;
-        min = (min < 10) ? '0' + min : min;
-        sec = (sec < 10) ? '0' + sec : sec;
-        return (min + ':' + sec);
-        totalTime(Math.round(audio.duration));
+        songSlider.style.width = position * audio.duration + '%';
+        document.getElementById("songSlider") = audio.duration;
+
     }
-    function totalTime(seconds) {
-        var timeNow = document.getElementById('currentTime');
-        var min = Math.floor(seconds / 60);
-        var sec = seconds % 60;
+
+    function convertTime (secs) {
+        var min = Math.floor(secs/60);
+        var sec = secs % 60;
         min = (min < 10) ? '0' + min : min;
         sec = (sec < 10) ? '0' + sec : sec;
         return (min + ':' + sec);
-        timeNow.textContent += " / " + min + ":" + sec;
+    }
+    function seekSong() {
+        audio.currentTime = songSlider.value;
+        currentTime = convertTime(audio.currentTime);
+    }
+
+    function showDuration(){
+        $(audio).bind('timeupdate', function(){
+            //Get hours and minutes
+            var sec = parseInt(audio.currentTime % 60);
+            var min = parseInt((audio.currentTime / 60) % 60);
+            var secFull = parseInt(audio.duration % 60);
+            var minFull = parseInt((audio.duration / 60) % 60);
+            //Add 0 if seconds less than 10
+            if (sec < 10) {
+                sec = '0' + sec;
+            }
+            $('#duration').html(min + '.' + sec);	
+            var value = 0;
+            if (audio.currentTime > 0) {
+                value = Math.floor((100 / audio.duration) * audio.currentTime);
+            }
+            $('#durationFull').html('/ ' + minFull + '.' + secFull);
+            $('#progress').css('width',value+'%');
+        });
+    }
+    function Volume() {
+        var slider = document.getElementById('volume');
+        var output = document.getElementById('volumevalue');
+        var audio = document.getElementById('audio')
+        output.innerHTML = slider.nodeValue;
+
+        slider.onchange = function () {
+            output.innerHTML = this.value;
+            //Koska range on 0-10, mutta volume toimii 0-1, pitää siis jakaa 10, joten saadan esim. 5 / 10 = 0.5
+            audio.volume = this.value / 10;
+        }
     }
 	/* Play-toiminto */
 	function playMusic() {
         audio.play();
         $('#play').hide();
         $('#pause').show();
-        $('#duration').fadeIn(400);
         showDuration();
 	}
 
@@ -165,11 +184,4 @@ var Mediaplayer = function() {
         duration.textContent = convertTime(d)
     }
 */
-	function volumeUp() {
-		/* Lisää toiminnallisuus */
-	}
-
-	function volumeDown() {
-		/* Lisää toiminnallisuus */
-    }
 }
--- musiikkisoitin/server.js
+++ musiikkisoitin/server.js
@@ -1,46 +1,62 @@
 const express = require('express');
-const request = require('request');
-const Database = require('./model/database.js');
-const BodyParser = require('body-parser');
-const cors = require('cors');
+const app = express();
+const expressLayouts = require('express-ejs-layouts');
+const data = require('./kappaleet.json');
+const player = require('./player.js');
 
-var app = express();
-
-const connection = new Database();
+const indexRouter = require('./routes/index');
+//const connection = new Database();
 const port = 8080;
-// CORS-middlewaren käyttöönotto
-app.use(cors());
 
-app.use(BodyParser.json());
-app.use(BodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.set("vies", __dirname + "/views");
+app.set("layout", "layouts/layouts");
+app.use(expressLayouts);
+app.use(express.static("public"));
 // Sallitaan pääsy selaimen tarvitsemiin tiedostoihin
-app.use(express.static(__dirname + '/client'));
+//app.use(express.static(__dirname + '/client'));
 //Estää liian datan lukemisen servulle
-app.use(express.json({ limit: '1mb' }));
+app.use(express.json({ limit: '10mb' }));
 
-/*
-app.get('/api/v1/presidents', async (req, res) => {
-  try {
-  connection.getAll(function (presidents) {
-    res.send(presidents);
-  }, req.body);
-  } catch (err) {
-    res.status(400).send(err);
-  }
-});
-*/
-/*
-app.post('/api/v1/presidents', async (req, res) => {
-  console.log('POST /api/v1/presidents', req.body);
-  try {
-    connection.addPresidents(function (response) {
-      res.send(response);
-    }, req.body);
-    res.send('presidents added');
-  } catch (err) {
-    res.status(400).send(err);
-  }
-});
-*/
+app.use('/', indexRouter);
+
 
 app.listen(port, '0.0.0.0', () => console.log(`Listening on port ${port}!`));
+
+  /*
+  console.log("GET http://localhost:8080/musiikkisoitin");
+  console.log("eli client.js vaatii muutoksia...");
+  res.send(data);
+  
+
+  fetch(data)
+  .then(function(response){
+    return response.json();
+  })
+  .then(function(songs){
+    let html = '';
+
+    songs.forEach(function(song){
+      html += `
+        <li>
+          <a target="_blank" href="$
+          {song.link}">View
+          Image</a>
+          ${song.artist}
+        </li>
+      `;
+    });
+    document.getElementById('playlist').innerHTML =
+    html;
+  })
+  .catch(function(error) {
+    console.log(error);
+  })
+
+  function kappaleet()
+{
+    obj = JSON.parse(data);
+    document.getElementById("title").innerHTML = obj.kappaleet[1].title;
+    document.getElementById("artist").innerHTML = obj.kappaleet[1].artist;
+}
+  */
